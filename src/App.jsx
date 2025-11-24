f87418954ab9d38b5eac0ef78293dbd089ffb3f7
jorge <jorgesaal@gmail.com>
2025-11-24 11:14:30 -0300

    otro

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,9 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Home from './components/Home';
 import Ofertas from './components/Ofertas';
-import Infaltables from './components/Infaltables';
 
+
+import Infaltables from './components/Infaltables';
 import Login from './components/Login'; 
 import Footer from './components/Footer'
 import { CartProvider } from './components/CartContext';
